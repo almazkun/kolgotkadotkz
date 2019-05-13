8138371b12901765d549ca3a5fe10a641f8626c3
Almaz Kunpeissov <almaz.kun@gmail.com>
2019-05-13 10:51:10 +0600

    Templates, Htmls added

--- homepage/urls.py
+++ homepage/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 from .views import HomepageListView
 
 
-urlrpatterns = [
-    path('', HomepageListView.as_view(), name='home')
+urlpatterns = [
+    path('', HomepageListView.as_view(), name='home'),
 ]
