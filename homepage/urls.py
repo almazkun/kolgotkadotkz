7fd2475c1ac6a67125f69916daab75a608a5fd18
Almaz Kunpeissov <almaz.kun@gmail.com>
2019-05-13 17:23:00 +0600

    Landing page, posts, tests added

--- homepage/urls.py
+++ homepage/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
 
-from .views import HomepageListView
+from .views import HomepageListView, HomepageDetailView
 
 
 urlpatterns = [
+    path('post/<int:pk>/', HomepageDetailView.as_view(), name='post_detail'),
     path('', HomepageListView.as_view(), name='home'),
 ]
