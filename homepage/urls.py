676b5a6a1e6f3d3b7b7df54a9171400de77e0579
Almaz Kunpeissov <almaz.kun@gmail.com>
2019-05-15 13:19:12 +0600

    Adding, Editing, deleting posts functionality added

--- homepage/urls.py
+++ homepage/urls.py
@@ -1,10 +1,19 @@
 from django.urls import path
 
 
-from .views import HomepageListView, HomepageDetailView
+from .views import (
+                    HomepageListView, 
+                    HomepageDetailView, 
+                    HomepageCreateView,
+                    HomepageUpdateView,
+                    HomepageDeleteView,
+                    )
 
 
 urlpatterns = [
+    path('post/<int:pk>/delete/', HomepageDeleteView.as_view(), name='post_delete'),
+    path('post/<int:pk>/edit', HomepageUpdateView.as_view(), name='post_edit'),
+    path('post/new/', HomepageCreateView.as_view(), name='post_new'),
     path('post/<int:pk>/', HomepageDetailView.as_view(), name='post_detail'),
     path('', HomepageListView.as_view(), name='home'),
 ]
