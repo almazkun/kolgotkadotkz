d1da08f2483392ecf7c73fc6bfb9ec1de67967f3
Almaz Kunpeissov <almaz.kun@gmail.com>
2019-05-24 11:48:56 +0600

    Cart fuctrionality is added

--- kolgotkakz_project/urls.py
+++ kolgotkakz_project/urls.py
@@ -20,6 +20,7 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
+    path('cart/', include('cart.urls')),
     path('shop/', include('shop.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('accounts.urls')),
