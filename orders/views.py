abe61c5042d34f8e5a94f6ad402af78340bdcc25
Almaz Kunpeissov <almaz.kun@gmail.com>
2019-05-27 17:43:16 +0600

    Order, customer functionality added

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import OrderItem
+from .forms import OrderCreatedForm
+from cart.cart import Cart
 
-# Create your views here.
+
+def order_created(request):
+    cart = Cart(request)
+    if request.method == 'POST'
+        form = OrderCreatedForm(request.POST)
+        if form.is_valid():
+            order = form.save()
+            for item in cart:
+                OrderItem.objects.create(
+                    order=order,
+                    product=item['product']
+                    price=
+                )
